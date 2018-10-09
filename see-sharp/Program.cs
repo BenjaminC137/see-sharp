42ad86c239161c644b9ca34620594f06b6aa304e
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2018-10-09 16:15:12 -0500

    set up class

--- see-sharp/Program.cs
+++ see-sharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace see_sharp
 {
@@ -6,8 +7,31 @@ namespace see_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GPS a = new GPS(2,9,DateTime.Now);
+            
+            Console.WriteLine("Latitude = {0} \nLongitude = {1}", a.Latitude, a.Longitude);
+            
         }
+        
+        //public GPS DisplayInfo(string coord){
+        //return true;
+        //}
+    }
+    
+   public class GPS
+    {
+        public int      Latitude    { get; set; }
+        public int      Longitude   { get; set; }
+        public DateTime Time        { get; set; }
+        
+        public GPS(int latitude, int longitude, DateTime time)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Time = time;
+        }
+        
+        
     }
 }
 
