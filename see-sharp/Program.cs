970713896b7ffa3a7551423285127026d8957d8b
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2018-10-09 15:28:07 -0500

    initial

---
+++ see-sharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace see_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+//Dredges use a GPS Device for navigation which sends out strings that are useful for our positioning programs, one of which is a NMEA GGA string.
+//A NMEAA GGA string contains various information such as time, latitude, longitude, altitude
+//The challenge is to write a code using C# that can parse a string input and be able to output the data (time, latitude, longitude, etc.)
