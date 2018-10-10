e333a015b55c48ae7fa8b2ba2be690dae1c18988
Benjamin <40469901+BenjaminC137@users.noreply.github.com>
2018-10-09 20:43:14 -0500

    added time formatting

--- see-sharp/Program.cs
+++ see-sharp/Program.cs
@@ -8,44 +8,49 @@ namespace see_sharp
         static void Main(string[] args)
         {
         
-            GPS point = new GPS();
-            GPS point = DisplayInfo("alsdkfjalk");
-            
+            GPS point = new GPS("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
+            //GPS point = DisplayInfo("alsdkfjalk");
             
             //GPS point = new GPS(2,9,DateTime.Now);
-            Console.WriteLine("Latitude = {0} \nLongitude = {1} \nTime = {2}", point.Latitude, point.Longitude, point.Time);
-            Console.ReadKey();
-            
-            
-            
+            Console.WriteLine("Latitude = {0} \nLongitude = {1} \nTime = {2}", point.Latitude, point.Longitude, point.Time);            
         }
   
-        public GPS DisplayInfo(string coord){
-        
-        int pLatitude = 3; 
-        int pLongitude = 4;
-        DateTime pTime = DateTime.Now;
-        
-        GPS p = new GPS(pLatitude, pLongitude, pTime);
-        
-        
-        return p;
-        }      
     }
     
    public class GPS
     {
-        public int      Latitude    { get; set; }
-        public int      Longitude   { get; set; }
-        public DateTime Time        { get; set; }
+        public decimal  Latitude    { get; set; }
+        public decimal  Longitude   { get; set; }
+        public string   Time        { get; set; }
         
-        public GPS(int latitude, int longitude, DateTime time)
-        {
-            Latitude = latitude;
-            Longitude = longitude;
-            Time = time;
-        }
+        //public GPS(int latitude, int longitude, DateTime time)
+        //{
+        //    Latitude = latitude;
+        //    Longitude = longitude;
+        //    Time = time;
+        //}
+        
         public GPS(string coord)
+        {
+            string[] NMEA = coord.Split(',');
+            decimal pLatitude = Convert.ToDecimal(NMEA[2]); 
+            decimal pLongitude = Convert.ToDecimal(NMEA[4]);          
+            string time = NMEA[1];
+            string hh = time.Substring(0,2);
+            string mm = time.Substring(2,2);
+            string ss = time.Substring(4,2);
+            string formattedTime = String.Format("{0}:{1}:{2}", hh, mm, ss);
+            //Console.WriteLine(formattedTime);
+            //DateTime pDateAndTime = DateTime.Parse(formattedTime);
+            //DateTime pTime = pDateAndTime;
+            Latitude = pLatitude;
+            Longitude = pLongitude;
+            Time = formattedTime;
+            //GPS p = new GPS(pLatitude, pLongitude, pTime);
+            
+            //return p;
+        //public GPS DisplayInfo(string coord){ 
+        }
         
         
     }
